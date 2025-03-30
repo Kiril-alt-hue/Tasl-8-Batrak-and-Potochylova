c7aac1c1bbca738842c7e57cfa6e0454636bbbb8
Kiril-alt-hue <kirabatrak@gmail.com>
2025-03-30 13:28:47 +0300

    ClockFace - перерлобив як "розширений" виібр годинників

--- 241/ClockFace.py
+++ 241/ClockFace.py
@@ -1,42 +1,48 @@
-from Digits import *
+
 from Hand import *
 
 
 class ClockFace:
     def __init__(self, radius=200):
         self.radius = radius
-        self.digits = []
-        for i in range(1, 13):
-            digit = Digit(i, radius * 0.8)
-            self.digits.append(digit)
-        self.hands = Hand(radius)
+        self.analog_watch = AnalogWatch(radius)
+        self.digital_watch = DigitalWatch(radius)
+        self.current_watch = self.analog_watch  # default to analog
+        self.screen = turtle.Screen()
+        self.setup_key_bindings()
+
+    def setup_key_bindings(self):
+        self.screen.onkey(self.toggle_watch, "w")  # 'w' to switch watch type
+        self.screen.onkey(self.toggle_format, "f")  # 'f' to toggle 12/24 hour format
+        self.screen.listen()
+
+    def toggle_watch(self):
+        if isinstance(self.current_watch, AnalogWatch):
+            self.current_watch = self.digital_watch
+        else:
+            self.current_watch = self.analog_watch
+        self.screen.clear()
+        self.current_watch.draw()
+
+    def toggle_format(self):
+        if isinstance(self.current_watch, DigitalWatch):
+            self.current_watch.toggle_format()
 
     def draw(self):
-        t = turtle.Turtle()
-        t.hideturtle()
-        t.speed(0)
-        t.penup()
-        t.goto(0, -self.radius)
-        t.pendown()
-        t.circle(self.radius)
+        self.current_watch.draw()
 
-
-        for digit in self.digits:
-            digit.draw()
-
-        #малюємо стрілки
-        self.hands.update()
+    def update(self):
+        self.current_watch.update()
 
 
 def main():
     screen = turtle.Screen()
-    screen.title("Простий годинник")
+    screen.title("Розширений годинник")
     clock = ClockFace(200)
     clock.draw()
 
-
     while True:
-        clock.hands.update()
+        clock.update()
         screen.update()
         time.sleep(1)
 
